09c5b9805fa374922c4db8e08d2b7832e9764670
Steve Campbell <sdcampbell68@live.com>
2023-12-04 13:24:48 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # nmapurls
 Nmapurls parses Nmap xml reports from either piped input or command line arg and outputs a list of http(s) URL's to be used in an automation pipeline.
+
+## Usage:
+
+```
+cat nmap.xml | ./nmapurls
+http://192.168.1.1:80
+https://192.168.1.1:443
+http://192.168.1.4:80
+https://192.168.1.4:443
+http://192.168.1.4:8080
+http://192.168.1.6:80
+https://192.168.1.6:443
+http://192.168.1.7:80
+https://192.168.1.7:443
+http://192.168.1.13:80
+https://192.168.1.13:443
+```
+
+```
+./nmap -f nmap.xml
+http://192.168.1.1:80
+https://192.168.1.1:443
+http://192.168.1.4:80
+https://192.168.1.4:443
+http://192.168.1.4:8080
+http://192.168.1.6:80
+https://192.168.1.6:443
+http://192.168.1.7:80
+https://192.168.1.7:443
+http://192.168.1.13:80
+https://192.168.1.13:443
+```
+
+## Build:
+
+You must already have go installed, then:
+
+```
+git clone https://github.com/sdcampbell/nmapurls.git
+cd nmapurls
+go build nmapurls.go
+```
---
+++ nmapurls.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"encoding/xml"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+type NmapRun struct {
+	XMLName xml.Name `xml:"nmaprun"`
+	Hosts   []Host   `xml:"host"`
+}
+
+type Host struct {
+	Addresses []Address `xml:"address"`
+	Ports     Ports     `xml:"ports"`
+}
+
+type Address struct {
+	Addr     string `xml:"addr,attr"`
+	AddrType string `xml:"addrtype,attr"`
+}
+
+type Ports struct {
+	Port []Port `xml:"port"`
+}
+
+type Port struct {
+	Protocol string  `xml:"protocol,attr"`
+	PortID   int     `xml:"portid,attr"`
+	State    State   `xml:"state"`
+	Service  Service `xml:"service"`
+}
+
+type State struct {
+	State string `xml:"state,attr"`
+}
+
+type Service struct {
+	Name string `xml:"name,attr"`
+}
+
+func parseNmapXML(reader io.Reader) (*NmapRun, error) {
+	bytes, err := ioutil.ReadAll(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	var nmapRun NmapRun
+	err = xml.Unmarshal(bytes, &nmapRun)
+	if err != nil {
+		return nil, err
+	}
+
+	return &nmapRun, nil
+}
+
+func main() {
+	fileFlag := flag.String("file", "", "Nmap XML report file path")
+	flag.StringVar(fileFlag, "f", "", "Nmap XML report file path (shorthand)")
+	flag.Parse()
+
+	var reader io.Reader
+
+	if *fileFlag != "" {
+		xmlFile, err := os.Open(*fileFlag)
+		if err != nil {
+			fmt.Println("Error opening file:", err)
+			return
+		}
+		defer xmlFile.Close()
+		reader = xmlFile
+	} else {
+		reader = os.Stdin
+	}
+
+	nmapRun, err := parseNmapXML(reader)
+	if err != nil {
+		fmt.Println("Error parsing XML:", err)
+		return
+	}
+
+	for _, host := range nmapRun.Hosts {
+		var ip string
+		for _, addr := range host.Addresses {
+			if addr.AddrType == "ipv4" {
+				ip = addr.Addr
+				break
+			}
+		}
+
+		for _, port := range host.Ports.Port {
+			if port.State.State == "open" {
+				protocol := "http"
+				if port.Service.Name == "https" || strings.Contains(port.Service.Name, "ssl") || port.PortID == 443 {
+					protocol = "https"
+				}
+
+				url := fmt.Sprintf("%s://%s:%d", protocol, ip, port.PortID)
+				fmt.Println(url)
+			}
+		}
+	}
+}
+
